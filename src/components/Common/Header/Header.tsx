358154f710277b9b37db37d7947329c146b9f96b
andrsweb <d3vv.proo@gmail.com>
2025-11-10 18:25:45 +0700

    feat: add header component

---
+++ src/components/Common/Header/Header.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import s from './Header.module.scss';
+import Link from 'next/link';
+import Image from 'next/image';
+import Button  from '../../Ui/Button/Button';
+import Container  from '../Container/Container';
+
+export default function Header()
+{
+    return (
+        <header className={s.header}>
+	        <Container maxWidth={1340}>
+		        <div className={s.headerWrapper}>
+			        <Link href="/">
+				        <Image src="/img/svg/logo.svg" width={176} height={50} alt="Vector logotype MovieMe"/>
+			        </Link>
+			        <Button color="violet" type="button">Sign in</Button>
+		        </div>
+	        </Container>
+        </header>
+    )
+}
