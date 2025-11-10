cb6bc15679144119e56ebcc81f1a59f63e93a9fe
andrsweb <d3vv.proo@gmail.com>
2025-11-10 18:25:29 +0700

    feat: add button/link component

---
+++ src/components/Ui/Button/Button.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import React from 'react'
+import Link from 'next/link'
+import clsx from 'clsx'
+import s from './Button.module.scss'
+
+interface ButtonProps {
+	children: React.ReactNode
+	className?: string
+	href?: string
+	onClick?: () => void
+	color: 'violet' | 'transparent'
+	type?: 'button' | 'submit' | 'reset'
+}
+
+export default function Button({ children, className, href, onClick, color, type = 'button', }: ButtonProps) {
+	const classes = clsx(s.button, s[color], className)
+
+	if (href) {
+		return (
+			<Link href={href} className={classes}>
+				{children}
+			</Link>
+		)
+	}
+
+	return (
+		<button type={type} onClick={onClick} className={classes}>
+			{children}
+		</button>
+	)
+}
