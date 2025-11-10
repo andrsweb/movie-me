457f69b365d98a6ae20d8cac893111727b39934a
andrsweb <d3vv.proo@gmail.com>
2025-11-10 16:09:55 +0700

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import type { Metadata } from "next";
+import { Instrument_Sans } from "next/font/google";
+import "../scss/main.scss";
+
+const instrumentSans = Instrument_Sans({
+	variable: "--ff-instrument-sans",
+	weight: ["400", "700"],
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "MovieMe | World's First Pay-Per-Minute Movie Streaming",
+	description: "Stream movies your way with MovieMe, the world's first pay-per-minute service. No subscriptions, " +
+		"no hidden fees—watch anytime on mobile, web & beyond today!",
+};
+
+export default function RootLayout({
+	                                   children,
+                                   }: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+		<body className={`${instrumentSans.variable}`}>
+		{children}
+		</body>
+		</html>
+	);
+}
