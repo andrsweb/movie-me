d40784961e070375b7f766f96231c352a96e0ac0
andrsweb <d3vv.proo@gmail.com>
2025-11-13 04:03:41 +0700

    upd: header button anim

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -1,23 +1,44 @@
 "use client"
 
+import { useState, useEffect } from 'react';
+import clsx from 'clsx';
 import s from './Header.module.scss';
 import Link from 'next/link';
 import Image from 'next/image';
-import Button  from '../../Ui/Button/Button';
-import Container  from '../Container/Container';
+import Button from '../../Ui/Button/Button';
+import Container from '../Container/Container';
+
+const SCROLL_THRESHOLD = 50;
 
 export default function Header()
 {
-    return (
-        <header className={s.header}>
-	        <Container maxWidth={1540}>
-		        <div className={s.headerWrapper}>
-			        <Link href="/">
-				        <Image src="/img/svg/logo.svg" width={176} height={50} alt="Vector logotype MovieMe"/>
-			        </Link>
-			        <Button color="violet" type="button">Sign in</Button>
-		        </div>
-	        </Container>
-        </header>
-    )
+	const [isScrolled, setIsScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const shouldBeScrolled = window.scrollY > SCROLL_THRESHOLD;
+			setIsScrolled(shouldBeScrolled);
+		};
+
+		handleScroll();
+
+		window.addEventListener('scroll', handleScroll, { passive: true });
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
+
+	return (
+		<header className={s.header}>
+			<Container maxWidth={1540}>
+				<div className={s.headerWrapper}>
+					<Link href="/">
+						<Image src="/img/svg/logo.svg" width={176} height={50} alt="Vector logotype MovieMe"/>
+					</Link>
+					<div className={clsx(s.headerButton, isScrolled && s.scrolled)}>
+						<Button color="violet" href="/download">Download</Button>
+					</div>
+					<Button className={s.headerLink} color="violet" type="button">Sign in</Button>
+				</div>
+			</Container>
+		</header>
+	)
 }
