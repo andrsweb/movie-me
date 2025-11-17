ab56c3675919417b9c6c17cd90a0d17bd355f5bb
andrsweb <d3vv.proo@gmail.com>
2025-11-17 10:27:21 +0700

    feat: open dropdowns on hover

--- src/components/Sections/Dropdowns/Dropdowns.tsx
+++ src/components/Sections/Dropdowns/Dropdowns.tsx
@@ -41,6 +41,14 @@ export default function Dropdowns() {
 		}
 	}
 
+	const handleMouseEnter = (id: number) => {
+		setActiveDropdown(id)
+	}
+
+	const handleMouseLeave = () => {
+		setActiveDropdown(null)
+	}
+
 	return (
 		<section className={s.dropdowns}>
 			<Container maxWidth={1400}>
@@ -51,6 +59,8 @@ export default function Dropdowns() {
 							className={clsx(s.dropdownsItem, {
 								[s.active]: activeDropdown === item.id
 							})}
+							onMouseEnter={() => handleMouseEnter(item.id)}
+							onMouseLeave={handleMouseLeave}
 						>
 							<button 
 								className={s.dropdownsHeader}
