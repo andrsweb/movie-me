2bb52f9e11cab42ca6dc84e4485fe2f5fcd49a0f
andrsweb <d3vv.proo@gmail.com>
2025-11-17 14:59:03 +0700

    upd: comment steps section

--- src/Clients/HomeClient/HomeClient.tsx
+++ src/Clients/HomeClient/HomeClient.tsx
@@ -3,7 +3,7 @@ import MoviePreview from "@/components/Sections/MoviePreview/MoviePreview";
 import About from "@/components/Sections/About/About";
 import Bill from "@/components/Sections/Bill/Bill";
 import Fly from "@/components/Sections/Fly/Fly";
-import Steps from "@/components/Sections/Steps/Steps";
+// import Steps from "@/components/Sections/Steps/Steps";
 import Dropdowns from "@/components/Sections/Dropdowns/Dropdowns";
 import Cta from "@/components/Sections/Cta/Cta";
 import AnimatedText from "@/components/Sections/AnimatedText/AnimatedText";
@@ -18,7 +18,7 @@ export default function HomeClient() {
 			<About/>
 			<Bill/>
 			<Fly />
-			<Steps />
+			{/*<Steps />*/}
 			<Dropdowns />
 			<Cta />
 			<AnimatedText />
