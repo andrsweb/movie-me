e7ee4f198a7e991a2dfe5d739dea2852869141ba
andrsweb <d3vv.proo@gmail.com>
2025-11-17 10:01:17 +0700

    upd: simlify mask

--- src/lib/animations.ts
+++ src/lib/animations.ts
@@ -1,15 +1,25 @@
 export const maskTextVariants = {
 	visible: {
 		opacity: 1,
+		x: 0,
+		y: 0,
+		maskImage: 'linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 100%)',
+		maskSize: '100% 100%',
+		WebkitMaskImage: 'linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 100%)',
+		WebkitMaskSize: '100% 100%',
+		maskPosition: '0 0%',
+		WebkitMaskPosition: '0 0%',
 	},
 	hidden: {
 		opacity: 0,
-		maskImage: 'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0) 30%, rgba(0,0,0,0.3) 50%, rgba(0,0,0,1) 70%, rgba(0,0,0,1) 100%)',
-		maskSize: '100% 200%',
-		WebkitMaskImage: 'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0) 30%, rgba(0,0,0,0.3) 50%, rgba(0,0,0,1) 70%, rgba(0,0,0,1) 100%)',
-		WebkitMaskSize: '100% 200%',
-		maskPosition: '0 100%',
-		WebkitMaskPosition: '0 100%',
+		x: 30,
+		y: 10,
+		maskImage: 'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 100%)',
+		maskSize: '100% 100%',
+		WebkitMaskImage: 'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 100%)',
+		WebkitMaskSize: '100% 100%',
+		maskPosition: '0 0%',
+		WebkitMaskPosition: '0 0%',
 	}
 }
 
