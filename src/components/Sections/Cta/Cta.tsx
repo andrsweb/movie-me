c62afa1183c8737b69a84ade9729d1b47a42aacb
andrsweb <d3vv.proo@gmail.com>
2025-11-29 13:10:43 +0700

    upd: cta with tailwind

--- src/components/Sections/Cta/Cta.tsx
+++ src/components/Sections/Cta/Cta.tsx
@@ -2,7 +2,6 @@
 
 import { useRef } from 'react'
 import { motion, useScroll, useTransform } from 'framer-motion'
-import s from './Cta.module.scss'
 import Container from '@/components/Common/Container/Container'
 import Image from "next/image";
 import Link from "next/link";
@@ -21,7 +20,7 @@ export default function Cta() {
 	return (
 		<motion.section 
 			ref={sectionRef}
-			className={s.cta}
+			className="w-full py-[60px] md:py-[100px]"
 			style={{ 
 				opacity: sectionOpacity,
 				y: sectionY
@@ -32,17 +31,22 @@ export default function Cta() {
 				plus you
 			</h2>
 			<Container maxWidth={1164}>
-				<div className={s.ctaInner}>
-					<h3><em>100,000+</em> users,
-						plus you</h3>
-					<p>
+				<div className="w-full flex flex-col items-center gap-[30px] mx-auto py-[50px] px-[10px] rounded-[40px] bg-[#7E90B9] md:py-[50px] md:px-[50px]">
+					<h3 className="font-bold text-[50px] leading-[60px] text-center text-[var(--color-black)] md:text-[100px] md:leading-[120px]">
+						<em className="inline-block w-[220px] relative z-[1] md:w-[455px]">
+							100,000+
+							<span className="absolute left-1/2 bottom-[5%] block w-[101%] h-[15%] opacity-100 bg-[var(--color-blue)] z-[-1] md:h-[20%] md:bottom-[15%]" style={{ transform: 'translateX(-50%)' }} />
+						</em> users,
+						plus you
+					</h3>
+					<p className="font-bold text-[18px] leading-[22px] text-center text-[var(--color-black)] md:text-[32px] md:leading-[48px]">
 						It only takes a few minutes to get started
 					</p>
-					<div className={s.ctaLinks}>
-						<Link href="https://play.google.com/store/apps/details?id=com.movieme.app&hl=en" target="_blank" rel="noopener noreferrer">
+					<div className="flex items-center gap-[20px]">
+						<Link href="https://play.google.com/store/apps/details?id=com.movieme.app&hl=en" target="_blank" rel="noopener noreferrer" className="transition-opacity ease-in-out hover:opacity-70">
 							<Image src="/img/svg/app-store.svg" width={159} height={46} alt="Apple store logo" />
 						</Link>
-						<Link href="https://apps.apple.com/in/app/movieme/id6445994785" target="_blank" rel="noopener noreferrer">
+						<Link href="https://apps.apple.com/in/app/movieme/id6445994785" target="_blank" rel="noopener noreferrer" className="transition-opacity ease-in-out hover:opacity-70">
 							<Image src="/img/svg/google-play.svg" width={159} height={46} alt="Google Play store logo" />
 						</Link>
 					</div>
