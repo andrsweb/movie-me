b6b6d951832847e31bc8ab25a02cef0ecf239242
andrsweb <d3vv.proo@gmail.com>
2025-11-29 11:02:52 +0700

    upd: header on tailwind

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -1,8 +1,6 @@
 "use client"
 
 import { useState, useEffect } from 'react';
-import clsx from 'clsx';
-import s from './Header.module.scss';
 import Link from 'next/link';
 import Image from 'next/image';
 import Button from '../../Ui/Button/Button';
@@ -10,8 +8,7 @@ import Container from '../Container/Container';
 
 const SCROLL_THRESHOLD = 50;
 
-export default function Header()
-{
+export default function Header() {
 	const [isScrolled, setIsScrolled] = useState(false);
 
 	useEffect(() => {
@@ -26,17 +23,21 @@ export default function Header()
 		return () => window.removeEventListener('scroll', handleScroll);
 	}, []);
 
+	const headerButtonClasses = isScrolled
+		? 'fixed flex right-1/2 bottom-[30px] translate-x-1/2 text-center transition-all ease-in-out duration-[200ms] xl:hidden'
+		: 'fixed flex right-[20px] bottom-[calc(100%-66px)] md:right-[50px] md:bottom-[calc(100%-76px)] text-center transition-all ease-in-out duration-[200ms] xl:hidden';
+
 	return (
-		<header className={s.header}>
+		<header className="w-full absolute left-0 top-0 py-[30px] z-[100]">
 			<Container maxWidth={1540}>
-				<div className={s.headerWrapper}>
-					<Link className={s.headerLogo} href="/">
-						<Image src="/img/svg/logo.svg" width={176} height={50} alt="Vector logotype MovieMe"/>
+				<div className="flex items-center justify-between gap-[30px]">
+					<Link className="block w-[110px] md:w-[176px]" href="/">
+						<Image src="/img/svg/logo.svg" width={176} height={50} alt="Vector logotype MovieMe" />
 					</Link>
-					<div className={clsx(s.headerButton, isScrolled && s.scrolled)}>
+					<div className={headerButtonClasses}>
 						<Button color="violet" href="/download">Download App</Button>
 					</div>
-					<Button className={s.headerLink} color="violet" type="button">Sign in</Button>
+					<Button className="!hidden xl:!flex" color="violet" type="button">Sign in</Button>
 				</div>
 			</Container>
 		</header>
