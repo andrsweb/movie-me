ae39a497a47ccfcced69dc76694b4ed1f4cce90c
andrsweb <d3vv.proo@gmail.com>
2025-11-29 11:30:46 +0700

    upd: hero on tailwind

--- src/components/Sections/Hero/Hero.tsx
+++ src/components/Sections/Hero/Hero.tsx
@@ -1,18 +1,17 @@
 "use client"
 
 import HeroCard from "./HeroCard/HeroCard";
-import s from './Hero.module.scss'
 
 export default function Hero() {
-	return(
-			<section className={s.hero}>
-				<h1 className="sr-only">
-					Stream movies your way with MovieMe, the world&#39;s first pay-per-minute service.
-					No subscriptions, no hidden fees—watch anytime on mobile, web & beyond today!
-				</h1>
-				<div className={s.heroWrapper}>
-					<HeroCard />
-				</div>
-			</section>
-		)
+	return (
+		<section className="relative z-[50] w-full bg-[var(--color-dark)]">
+			<h1 className="sr-only">
+				Stream movies your way with MovieMe, the world&#39;s first pay-per-minute service.
+				No subscriptions, no hidden fees—watch anytime on mobile, web & beyond today!
+			</h1>
+			<div className="relative mx-auto flex h-full w-full flex-col items-start justify-end">
+				<HeroCard />
+			</div>
+		</section>
+	)
 }
