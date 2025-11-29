ed8193d70a97781d5d3a750a94b370d88cbb70dd
andrsweb <d3vv.proo@gmail.com>
2025-11-29 11:08:02 +0700

    upd: return classname in container

--- src/components/Common/Container/Container.tsx
+++ src/components/Common/Container/Container.tsx
@@ -6,10 +6,14 @@ import clsx from 'clsx'
 interface ContainerProps {
 	children: React.ReactNode
 	maxWidth: number
+	className?: string
 }
 
-export default function Container({ children, maxWidth }: ContainerProps) {
-    const classes = clsx('w-full mx-auto px-[20px] md:px-[50px]')
+export default function Container({ children, maxWidth, className }: ContainerProps) {
+    const classes = clsx(
+        'w-full mx-auto px-[20px] md:px-[50px]',
+        className,
+    )
 
 	return (
 		<div className={classes} style={{ maxWidth: `${maxWidth}px` }}>
