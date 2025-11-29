798d1c7dbcbd7bb395574e37914c64e19a700240
andrsweb <d3vv.proo@gmail.com>
2025-11-29 15:57:37 +0700

    upd: edit movie type

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -2,7 +2,15 @@ export interface Movie {
 	id: number
 	title: string
 	src: string
+    trailer: string
 	price: number
 	year: number
 	genre: string
+	slug: string
+	description: string
+	duration: number
+	rating: number
+	director: string
+	cast: string[]
+	categories: string[]
 }
