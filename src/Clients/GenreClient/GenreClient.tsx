0d9f8fba3968348cebcf947f59f5099cc68a40c4
andrsweb <d3vv.proo@gmail.com>
2025-12-01 01:55:22 +0700

    feat: create genre client

---
+++ src/Clients/GenreClient/GenreClient.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import type { Movie } from "@/types/movie"
+
+import GenreHero from "@/components/Sections/Genre/GenreHero/GenreHero"
+import GenreItems from "@/components/Sections/Genre/GenreItems/GenreItems"
+
+type BreadcrumbItem = {
+	label: string
+	href?: string
+}
+
+type RelatedGenre = {
+	name: string
+	slug: string
+	posters: Movie[]
+}
+
+interface GenreClientProps {
+	title: string
+	movieCount: number
+	movies: Movie[]
+	relatedGenres: RelatedGenre[]
+	breadcrumbs: BreadcrumbItem[]
+}
+
+export default function GenreClient({
+	title,
+	movies,
+	relatedGenres,
+	breadcrumbs,
+}: GenreClientProps) {
+	return (
+		<div className="w-full bg-[var(--color-dark)]">
+			<GenreHero
+				breadcrumbs={breadcrumbs}
+				title={`${title} Movies`}
+			/>
+			<GenreItems movies={movies} related={relatedGenres} />
+		</div>
+	)
+}
